d3bfa1fde04f949a56101a0954bf241d44fb8b3c
ryuyb <shijei2012@gmail.com>
2026-01-21 13:38:01 +0800

    feat: delete old tofu files

--- tofu/modules/cloudflare-r2/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "bucket_name" {
-  description = "R2 bucket name"
-  value       = cloudflare_r2_bucket.this.name
-}
-
-output "bucket_id" {
-  description = "R2 bucket ID"
-  value       = cloudflare_r2_bucket.this.id
-}
--- tofu/stacks/prod/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "dns_records" {
-  description = "Created DNS records"
-  value       = module.dns.records
-}
-
-output "velero_bucket" {
-  description = "Velero backup bucket name"
-  value       = module.velero_bucket.bucket_name
-}
